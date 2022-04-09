407e5544aeec9aa9e242087e6878c9a8a88dec84
kjain <kjain9071@hotmail.com>
2022-04-09 15:32:36 +0530

    new project data

---
+++ JS_1/Main.js
@@ -0,0 +1,47 @@
+﻿jQuery(function ($) {
+
+    var PARJAIN = window.PARJAIN || {};
+
+
+    /********************************
+            SCROLL TO TOP
+    *********************************/
+    PARJAIN.backToTop = function () {
+        var windowWidth = $(window).width(),
+            didScroll = false;
+
+        var $arrow = $('#back-to-top');
+
+        $arrow.click(function (e) {
+            $('body,html').animate({ scrollTop: "0" }, 700);
+            e.preventDefault();
+        })
+
+        $(window).scroll(function () {
+            didScroll = true;
+        });
+
+        setInterval(function () {
+            if (didScroll) {
+                didScroll = false;
+
+                if ($(window).scrollTop() > 100) {
+                    $arrow.css('display', 'block');
+                } else {
+                    $arrow.css('display', 'none');
+                }
+            }
+        }, 250);
+    }
+
+
+
+
+    /********************************
+            INITIALIZATION
+    *********************************/
+    $(document).ready(function () {
+        PARJAIN.backToTop();
+    });
+
+});
